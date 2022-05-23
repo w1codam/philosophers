d8fd1d79ee3ea89f2e379f90ed1c05612aba51fb
w1codam <jde-groo@f0r1s11.codam.nl>
2022-05-23 12:16:24 +0200

    header and main

---
+++ include/philosophers.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   philosophers.h                                     :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: jde-groo <jde-groo@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/05/23 12:14:34 by jde-groo      #+#    #+#                 */
+/*   Updated: 2022/05/23 12:15:39 by jde-groo      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <limits.h>
+# include <stdbool.h>
+
+#endif
